f8892343b4101b5d09add1934de315101d8e786f
unknown <vivekkumbhar3310@gmail.com>
2025-08-23 11:52:06 +0530

    create yout own library

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,14 @@
-
+import Chai from './chai';
 function App() {
+  const username = "chai or code"
 
   return (
-    <h1>Vivek kumbhar| vk</h1>
-      
+    <>
+    <Chai/>
+      <h1>chai or react {username}</h1>
+    <p>test para</p>
+    </>
   )
-}
+} 
 
 export default App
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,30 @@ import ReactDOM from 'react-dom/client'
 
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+    <div>
+      <h1>Custom App | Vivek</h1>
+    </div>
+  )
+}
+
+// Example JSX element
+const anotherElement = (
+  <a href="https://google.com" target="_blank">Visit Google</a>
+)
+
+const anotherUser = "chai or react"
+
+// Example using React.createElement
+const ReactElement = React.createElement(
+  'a',
+  { href: 'https://www.google.com', target: '_blank' },
+  'Click me to visit Google - ',
+  anotherElement
+
+)
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  ReactElement   
 )
